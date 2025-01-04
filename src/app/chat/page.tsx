8b7c87fff3843a499db93dfbb61dadc5f736079b
Hieu Lai <ldmhieudev@gmail.com>
2025-01-04 22:45:21 +0700

    feat: add ChatView component and update SideBar layout

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,9 +1,11 @@
 import { SideBar } from "@/components/containers/layout";
+import { ChatView } from "@/components/containers/list-chat";
 
 const ChatPage = () => {
 	return (
 		<>
 			<SideBar />
+			<ChatView />
 		</>
 	);
 };
--- src/components/containers/layout/sidebar.tsx
+++ src/components/containers/layout/sidebar.tsx
@@ -4,7 +4,7 @@ import { ChatIcon, CloudIcon, ContactIcon, SettingIcon } from "../../../../publi
 
 export const SideBar = () => {
 	return (
-		<div className="w-[92px] h-lvh bg-[#1384fd] py-2 px-3 flex flex-col justify-between">
+		<div className="w-[60px] h-lvh bg-[#1384fd] py-2 px-3 flex flex-col justify-between">
 			{/* Top side */}
 			<div className="justify-center flex flex-col gap-[18px]">
 				<Image
@@ -13,38 +13,38 @@ export const SideBar = () => {
 					width={68}
 					height={68}
 				></Image>
-				<div className="hover:bg-icon-active justify-center flex p-3 rounded-[8px] ">
+				<div className="hover:bg-icon-active justify-center flex p-1 rounded-[4px] ">
 					<ChatIcon
 						color="#fff"
-						size={36}
-						strokeWidth={5}
+						size={32}
+						strokeWidth={3}
 					/>
 				</div>
-				<div className="hover:bg-icon-active justify-center flex p-3 rounded-[8px] ">
+				<div className="hover:bg-icon-active justify-center flex p-1 rounded-[4px] ">
 					<ContactIcon
 						color="#fff"
-						size={36}
-						strokeWidth={5}
+						size={32}
+						strokeWidth={3}
 					/>
 				</div>
 			</div>
 			{/* Bottom side */}
 			<div className="justify-center flex flex-col gap-[18px]">
-				<div className=" justify-center flex  rounded-[8px]">
-					<div className="w-[52px] h-[6px] bg-white rounded-[8px]"></div>
+				<div className=" justify-center flex  rounded-[4px]">
+					<div className="w-[52px] h-[6px] bg-white rounded-[4px]"></div>
 				</div>
-				<div className="hover:bg-icon-active justify-center flex p-3 rounded-[8px] ">
+				<div className="hover:bg-icon-active justify-center flex p-1 rounded-[4px] ">
 					<CloudIcon
 						color="#fff"
-						size={36}
-						strokeWidth={5}
+						size={32}
+						strokeWidth={3}
 					/>
 				</div>
-				<div className="hover:bg-icon-active justify-center flex p-3 rounded-[8px] ">
+				<div className="hover:bg-icon-active justify-center flex p-1 rounded-[4px] ">
 					<SettingIcon
 						color="#fff"
-						size={36}
-						strokeWidth={5}
+						size={32}
+						strokeWidth={3}
 					/>
 				</div>
 			</div>
