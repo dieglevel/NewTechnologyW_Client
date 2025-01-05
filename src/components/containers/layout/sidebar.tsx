94d0d2c8103da098d9c49e57e391fda95f639115
Hieu Lai <ldmhieudev@gmail.com>
2025-01-05 17:01:57 +0700

    Refactor: Complete Sidebar

--- src/components/containers/layout/sidebar.tsx
+++ src/components/containers/layout/sidebar.tsx
@@ -1,31 +1,25 @@
 import { avatar } from "@/assets/images";
+import { ChatIcon, CloudIcon, ContactIcon, SettingIcon } from "@/assets/svgs";
 import Image from "next/image";
-import { ChatIcon, CloudIcon, ContactIcon, SettingIcon } from "../../../../public/svg";
 
 export const SideBar = () => {
 	return (
-		<div className="w-[60px] h-lvh bg-[#1384fd] py-2 px-3 flex flex-col justify-between">
+		<div className="w-[60px] h-lvh bg-[#1384fd] py-1 px-2 flex flex-col justify-between">
 			{/* Top side */}
 			<div className="justify-center flex flex-col gap-[18px]">
-				<Image
-					src={avatar}
-					alt="Avatar"
-					width={68}
-					height={68}
-				></Image>
-				<div className="hover:bg-icon-active justify-center flex p-1 rounded-[4px] ">
-					<ChatIcon
-						color="#fff"
-						size={32}
-						strokeWidth={3}
-					/>
+				<div>
+					<Image
+						src={avatar}
+						alt="Avatar"
+						width={68}
+						height={68}
+					></Image>
 				</div>
 				<div className="hover:bg-icon-active justify-center flex p-1 rounded-[4px] ">
-					<ContactIcon
-						color="#fff"
-						size={32}
-						strokeWidth={3}
-					/>
+					<ChatIcon className="size-8 text-icon-active stroke-[3px]" />
+				</div>
+				<div className="hover:bg-icon-active justify-center items-center flex p-1 rounded-[4px] ">
+					<ContactIcon className=" size-8 text-icon-active stroke-[3px]" />
 				</div>
 			</div>
 			{/* Bottom side */}
@@ -34,18 +28,10 @@ export const SideBar = () => {
 					<div className="w-[52px] h-[6px] bg-white rounded-[4px]"></div>
 				</div>
 				<div className="hover:bg-icon-active justify-center flex p-1 rounded-[4px] ">
-					<CloudIcon
-						color="#fff"
-						size={32}
-						strokeWidth={3}
-					/>
+					<CloudIcon className="size-8 text-icon-active stroke-[3px]" />
 				</div>
 				<div className="hover:bg-icon-active justify-center flex p-1 rounded-[4px] ">
-					<SettingIcon
-						color="#fff"
-						size={32}
-						strokeWidth={3}
-					/>
+					<SettingIcon className="size-8 text-icon-active stroke-[3px]" />
 				</div>
 			</div>
 		</div>
