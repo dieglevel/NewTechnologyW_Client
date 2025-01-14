82da43e83ea8770ac853bdfc66bff0fa8d4a295d
Phùng Anh Minh <dieglevel@gmail.com>
2025-01-14 16:14:39 +0700

    Reapply "Merge remote-tracking branch 'origin/loc'"
    This reverts commit c137630211a1ddaba61f7d61a77705d020b3e523.

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,4 @@
 import avatar from "./avatar.png";
+import avatarDefault from "./avatar-default.png";
 
-export { avatar };
+export { avatar, avatarDefault };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ export default {
 		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-		"./node_modules/@nextui-org/theme/dist/components/(button|image|input|link|ripple|spinner|form).js",
+		"./node_modules/@nextui-org/theme/dist/components/(button|image|input|input-otp|link|radio|ripple|spinner|form).js",
 	],
 	theme: {
 		extend: {
